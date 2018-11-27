792f7f5d5b08378aaea0169732d52118ffc6a2e1
Vincent Chee Jia Hong <cheehong05090@gmail.com>
2018-11-27 19:14:16 +0000

    update adapter comment

--- src/com/vincent/adapter_pattern/AdapterPatternDemo.java
+++ src/com/vincent/adapter_pattern/AdapterPatternDemo.java
@@ -4,6 +4,8 @@ public class AdapterPatternDemo {
     public static void main(String[] args) {
         AudioPlayer audioPlayer = new AudioPlayer();
 
+        // use of play method to provide abstraction (hiding implementation details)
+        // "play" is wired to more specific implementation (playVlc, playMp4) in MediaAdapter
         audioPlayer.play("mp3", "beyond the horizon.mp3");
         audioPlayer.play("mp4", "alone.mp4");
         audioPlayer.play("vlc", "far far away.vlc");
