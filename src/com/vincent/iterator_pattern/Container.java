10616cc6290594e019d710377cff302a99f30f3d
Vincent Chee Jia Hong <cheehong05090@gmail.com>
2018-12-04 23:52:03 +0000

    add iterator pattern

---
+++ src/com/vincent/iterator_pattern/Container.java
@@ -0,0 +1,5 @@
+package com.vincent.iterator_pattern;
+
+public interface Container {
+    public Iterator getIterator();
+}
---
+++ src/com/vincent/iterator_pattern/Iterator.java
@@ -0,0 +1,6 @@
+package com.vincent.iterator_pattern;
+
+public interface Iterator {
+    public boolean hasNext();
+    public Object next();
+}
