06200c4e6866875e75adf9c5e821af27cc0c8696
Vincent Chee Jia Hong <cheehong05090@gmail.com>
2018-12-05 17:07:29 +0000

    add comment in mvc pattern

--- src/com/vincent/mvc_pattern/Student.java
+++ src/com/vincent/mvc_pattern/Student.java
@@ -2,6 +2,9 @@ package com.vincent.mvc_pattern;
 
 /**
  * Depict the student model
+ * Student model contains the logic to alter the data field
+ * Controller will receive the control of the model, so that it alter the value using the logic
+ * View in turns will be notified by controller so that it will update the user
  */
 public class Student {
     private String rollNo;
